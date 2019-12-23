9046f565773cef78059ba688e9029e1e8b8fae68
Roberto Mardeni <rmardeni@gmail.com>
2019-12-23 10:27:54 -0500

    Added Console App

---
+++ src/EnterpriseJobScheduling/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Environment.GetEnvironmentVariable("ConsoleAppName");
+
+            Console.WriteLine($"Hello World, from {name}, at {DateTime.Now}!");
+        }
+    }
+}
